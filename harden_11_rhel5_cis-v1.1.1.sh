3097de406aa65f39d5f688436ca40dee3a6f7469
Jack Maynard <49212024+j-a-c-k-maynard@users.noreply.github.com>
2019-04-02 15:37:00 -0700

    Update harden_11_rhel5_cis-v1.1.1.sh

--- harden_11_rhel5_cis-v1.1.1.sh
+++ harden_11_rhel5_cis-v1.1.1.sh
@@ -3,8 +3,8 @@
 #
 # harden_11_rhel5_cis-v1.1.1.sh
 ##########################################################################
-# AUTHOR: HP Consulting - Security & Risk Management, Jack Maynard       #
-# Created: May 10, 2010 - HP version 1.0                                 #
+# AUTHOR: Jack Maynard                                                   #
+# Created: May 10, 2010 - version 1.0                                    #
 #                                                                        #
 # RHEL5 Hardening Script Section 11.0 - Misc Odds and Ends               #
 # Scripted to Center for Internet Security (CIS) RHEL5 Benchmark v1.1.1  #
